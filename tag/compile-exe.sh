d263a6d21a1ebd6f533d8bde83fcfeee4a788852
donginKim <steve1145@naver.com>
2018-10-31 14:49:41 +0900

    tags

--- tag/compile-exe.sh
+++ tag/compile-exe.sh
@@ -11,9 +11,9 @@ ls
 git remote -v
 
 git remote remove origin
-git remote add origin http://sudouser:sudouser@52.231.161.92:3000/sudouser/buildpack.git
+git remote add origin http://sudouser:sudouser@52.231.161.92:3000/$repo.git
 
-git tag v0.0.1
+git tag $tag
 
-git push -u origin v0.0.1
+git push -u origin $tag
 
