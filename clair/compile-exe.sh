d335cffece0f713bebc7d4826b992ca06ccf9fd5
donginKim <steve1145@naver.com>
2018-11-30 10:29:35 +0900

    ops

--- clair/compile-exe.sh
+++ clair/compile-exe.sh
@@ -10,5 +10,6 @@ cat /etc/resolv.conf
 
 ping -c 4 192.168.0.162
 ping -c 4 docker.io
+ping -c 4 8.8.8.8
 
 CLAIR_ADDR=http://192.168.0.162:6060 CLAIR_OUTPUT=High CLAIR_THRESHOLD=10 klar steve1145/complier:latest
