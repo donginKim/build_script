40965dc1150b4eec045c16eaafbbd63db484f6af
donginKim <steve1145@naver.com>
2018-11-30 10:22:47 +0900

    ops

--- clair/compile-exe.sh
+++ clair/compile-exe.sh
@@ -8,4 +8,4 @@ apt-get install -y iputils-ping
 ping -c 4 192.168.0.162
 ping -c 4 docker.io
 
-CLAIR_ADDR=http://192.168.0.162:6060 CLAIR_OUTPUT=High CLAIR_THRESHOLD=10 klar 192.168.0.162:5000/steve1145/complier
+CLAIR_ADDR=http://192.168.0.162:6060 CLAIR_OUTPUT=High CLAIR_THRESHOLD=10 klar steve1145/complier:latest
