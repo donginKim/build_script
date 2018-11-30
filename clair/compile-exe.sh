9dff51f8730c9f5e844751ae07911120950ec22d
donginKim <steve1145@naver.com>
2018-11-30 10:41:18 +0900

    l

--- clair/compile-exe.sh
+++ clair/compile-exe.sh
@@ -22,6 +22,8 @@ echo " ================= "
 
 apt-get install docker
 
-docker pull steve1145/complier:latest
+docker --version
+
+#docker pull steve1145/complier:latest
 
 #CLAIR_ADDR=http://192.168.0.162:6060 CLAIR_OUTPUT=High CLAIR_THRESHOLD=10 klar steve1145/complier:latest
