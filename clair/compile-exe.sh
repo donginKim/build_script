9521e84dc3ac71261511ebd8839a3156c235c5b5
donginKim <steve1145@naver.com>
2018-11-30 11:15:23 +0900

    ops

--- clair/compile-exe.sh
+++ clair/compile-exe.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-ls
-
-apt-get install -y iputils-ping
-
-echo " ================= "
-cat /etc/hosts
-echo " ================= "
-
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-
-echo " ================= "
-cat /etc/resolv.conf
-echo " ================= "
-
-echo " ================= "
-ping -c 4 192.168.0.162
-echo " ================= "
-ping -c 4 8.8.8.8
-echo " ================= "
-
-#apt-get install docker
-
-#docker --version
-
-#docker pull steve1145/complier:latest
-
 CLAIR_ADDR=http://192.168.0.162:6060 CLAIR_OUTPUT=High CLAIR_THRESHOLD=10 klar postgres:9.5.1
+
+#CLAIR_ADDR=$clair_address CLAIR_OUTPUT=High CLAIR_THRESHOLD=10 klar $scan_images
